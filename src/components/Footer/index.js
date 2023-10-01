6899092f01048601c2b2ed6a65f7a191fb5e0c92
chask96969 <160220a009@gmail.com>
2023-10-01 10:38:45 +0000

    Success

---
+++ src/components/Footer/index.js
@@ -0,0 +1,7 @@
+const Header = () => (
+  <div>
+    <h1>Footer</h1>
+  </div>
+)
+
+export default Header
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+// const Header = () => {
+//   return (
+//     <div>
+//       <h1>Header</h1>
+//     </div>
+//   )
+// }
+// export default Header
+
+const Header = () => (
+  <div>
+    <h1>Header</h1>
+  </div>
+)
+
+export default Header
